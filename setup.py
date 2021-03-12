0db151289a442f9348919e482a2382c067afcc82
travistune3 <80426231+travistune3@users.noreply.github.com>
2021-03-12 11:42:28 -0600

    included readme

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='multifil',
+      version='0.2',
+      description='A spatial half-sarcomere model and the means to run it',
+      url='https://github.com/cdw/multifil',
+      author='C David Williams',
+      author_email='cdave@uw.edu',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy', 'boto']
+     )
